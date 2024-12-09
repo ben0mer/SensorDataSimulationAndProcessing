309063f3cb7cba60c97742eb325b9c2c9c06db4a
Ömer Gökyer <123402995+ben0mer@users.noreply.github.com>
2024-12-09 14:38:57 +0300

    Proje dosyası ekle.

---
+++ SensorDataSimulationAndProcessing/SensorDataSimulationAndProcessing.cpp
@@ -0,0 +1,12 @@
+﻿// SensorDataSimulationAndProcessing.cpp: Uygulamanın giriş noktasını tanımlar.
+//
+
+#include "SensorDataSimulationAndProcessing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
